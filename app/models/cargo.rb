db77a97fba2ccf2266c47c1bbfe2869f57297440
sandra montoya <spmontoya20@misena.edu.co>
2016-12-01 20:38:02 +0000

    organizando detalles de eventos

--- app/models/cargo.rb
+++ app/models/cargo.rb
@@ -1,15 +1,16 @@
 class Cargo < ApplicationRecord
     
     def self.search(search, page)
-        where(['upper(nombre) like ?',
-        "%#{search}%".upcase]).paginate(page: page, per_page: 5).order("nombre")
+      where(['upper(nombre) like ?',
+      "%#{search}%".upcase]).paginate(page: page, per_page: 5).order("nombre")
     end
     
     # validamos que el campo no este vacio con presence
     validates :nombre , presence: true
     validates :dependencia , presence: true
     
-    def name
-        self.nombre
+    def name()
+      self.nombre
     end
+    
 end
